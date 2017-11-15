724a699c3a76747664f7c1ee60626991925ba4b4
darrenyip <darrenyipzy@gmail.com>
2017-11-15 08:38:01 -0500

    Initial commit

---
+++ No1/src/no1.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Darren on 11/15/2017.
+ */
+public class no1 {
+    public static void main(String[] args) {
+        int counter = 0;
+        for(int i=1; i<101; i++){
+
+            if(i%3==0 || i%5==0){
+                counter +=i;
+            }
+            System.out.println(counter);
+        }
+    }
+}
